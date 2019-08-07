4b7cb3106fd0be0a1eac79b81c9982a5ca8063d7
Leo SSH <267024412@qq.com>
2019-08-07 20:58:43 +0100

    2

--- run.sh
+++ run.sh
@@ -1,15 +1,27 @@
 #! /bin/bash
 
 
-python3 /home/leonardo/Project/TPTP-v7.2.0/test12.py
+python3 /Users/leo/Documents/leovampire/TPTP-v7.2.0/bucketting.py
+
 echo 'Bucketing Finished!';
 
+counter=0
 while read line
 do
+	now=$(date +"%T")
+	echo "Running test $counter at $now"
 	echo "STARTING NEW TEST" > result.txt
 	echo "$line" >> result.txt
-	$line>>result.txt
-	python3 read2.py $line
+	gtimeout 11 $line>>result.txt
+	python3 collect_results.py $line
+	#arr1=$( $line )
+	#arr2=$( echo ${arr1[1]} | tr "/" "\n" )
+	#arr3=$( $arr2 )
+	#problem=${arr3[9]}
+	#strat=${arr1[5]}
+	#mv result.txt "tmp/${problem}_${strat}"
+	counter=$((counter+1))
 	
-done < /home/leonardo/Project/TPTP-v7.2.0/commands.txt
+done < commands.txt
 
+echo 'Collection finished!'
